8b977098a3b8078dff76da95a5df90e55a442681
derMicha <dermicha@ubirch.com>
2020-04-26 15:28:07 +0200

    merged from master

--- src/main/scala/com/ubirch/receiver/conf/HeaderKeys.scala
+++ src/main/scala/com/ubirch/receiver/conf/HeaderKeys.scala
@@ -1,7 +1,13 @@
 package com.ubirch.receiver.conf
 
 object HeaderKeys {
-
-  val XUBIRCHCREDENTIAL = "X-Ubirch-Credential".toLowerCase
-
+  val XUBIRCHCREDENTIAL = "X-Ubirch-Credential".toLowerCase;
+  val XUBIRCHHARDWAREID = "X-Ubirch-Hardware-Id".toLowerCase;
+  val XUBIRCHAUTHTYPE = "X-Ubirch-Auth-Type".toLowerCase;
+  val AUTHORIZATION = "Authorization".toLowerCase;
+  val XXSRFTOKEN = "X-XSRF-TOKEN".toLowerCase;
+  val XCUMULOCITYBASEURL = "X-Cumulocity-BaseUrl".toLowerCase;
+  val XCUMULOCITYTENANT = "X-Cumulocity-Tenant".toLowerCase;
+  val XNIOMONPURGECACHES = "X-Niomon-Purge-Caches".toLowerCase;
+  val CONTENTTYPE = "Content-Type".toLowerCase;
 }
