0ef65d5fe22845547fb5b483a4df74440e3392ea
Carlos Sanchez <caaugustoss@gmail.com>
2020-08-21 11:24:22 +0200

    UP-2155 Removing x-niomon-purge-caches and removing ; (not needed in scala)

--- src/main/scala/com/ubirch/receiver/conf/HeaderKeys.scala
+++ src/main/scala/com/ubirch/receiver/conf/HeaderKeys.scala
@@ -1,13 +1,12 @@
 package com.ubirch.receiver.conf
 
 object HeaderKeys {
-  val XUBIRCHCREDENTIAL = "X-Ubirch-Credential".toLowerCase;
-  val XUBIRCHHARDWAREID = "X-Ubirch-Hardware-Id".toLowerCase;
-  val XUBIRCHAUTHTYPE = "X-Ubirch-Auth-Type".toLowerCase;
-  val AUTHORIZATION = "Authorization".toLowerCase;
-  val XXSRFTOKEN = "X-XSRF-TOKEN".toLowerCase;
-  val XCUMULOCITYBASEURL = "X-Cumulocity-BaseUrl".toLowerCase;
-  val XCUMULOCITYTENANT = "X-Cumulocity-Tenant".toLowerCase;
-  val XNIOMONPURGECACHES = "X-Niomon-Purge-Caches".toLowerCase;
-  val CONTENTTYPE = "Content-Type".toLowerCase;
+  val XUBIRCHCREDENTIAL = "X-Ubirch-Credential".toLowerCase
+  val XUBIRCHHARDWAREID = "X-Ubirch-Hardware-Id".toLowerCase
+  val XUBIRCHAUTHTYPE = "X-Ubirch-Auth-Type".toLowerCase
+  val AUTHORIZATION = "Authorization".toLowerCase
+  val XXSRFTOKEN = "X-XSRF-TOKEN".toLowerCase
+  val XCUMULOCITYBASEURL = "X-Cumulocity-BaseUrl".toLowerCase
+  val XCUMULOCITYTENANT = "X-Cumulocity-Tenant".toLowerCase
+  val CONTENTTYPE = "Content-Type".toLowerCase
 }
